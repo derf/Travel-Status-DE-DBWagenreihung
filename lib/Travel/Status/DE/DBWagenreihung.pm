c84338838750cd52a34c365964c983a45f18ae1c
Daniel Friesel <derf@finalrewind.org>
2023-02-13 17:31:27 +0100

    Release v0.08

--- lib/Travel/Status/DE/DBWagenreihung.pm
+++ lib/Travel/Status/DE/DBWagenreihung.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use 5.020;
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 use Carp qw(cluck confess);
 use JSON;
@@ -625,7 +625,7 @@ Travel::Status::DE::DBWagenreihung - Interface to Deutsche Bahn Wagon Order API.
 
 =head1 VERSION
 
-version 0.07
+version 0.08
 
 This is beta software. The API may change without notice.
 
